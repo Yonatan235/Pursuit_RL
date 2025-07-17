8e1982d59d47830943b087c6ab9852b116e25274
Yonatan L. Ashenafi <54382548+Yonatan235@users.noreply.github.com>
2025-07-17 11:02:38 -0400

    Create train.py

---
+++ train.py
@@ -0,0 +1,8 @@
+from stable_baselines3 import PPO
+from pursuit.env import PursuitEnv
+
+if __name__ == "__main__":
+    env = PursuitEnv()
+    model = PPO("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=20000)
+    model.save("models/ppo_pursuit")
